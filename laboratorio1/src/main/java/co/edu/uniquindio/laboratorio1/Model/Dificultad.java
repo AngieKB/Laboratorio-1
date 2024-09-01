879d7b674f8d9934c96a2c237d07462de4f2a0bb
AngieKB <142171986+AngieKB@users.noreply.github.com>
2024-09-01 16:21:44 -0500

    Delete laboratorio1 directory

--- laboratorio1/src/main/java/co/edu/uniquindio/laboratorio1/Model/Dificultad.java
+++
@@ -1,7 +0,0 @@
-package co.edu.uniquindio.laboratorio1.Model;
-
-public enum Dificultad {
-    BAJO,
-    MEDIO,
-    ALTO;
-}
--- laboratorio1/src/main/java/co/edu/uniquindio/laboratorio1/Model/TipoMiembro.java
+++
@@ -1,6 +0,0 @@
-package co.edu.uniquindio.laboratorio1.Model;
-
-public enum TipoMiembro {
-    JOVEN,
-    ADULTO
-}
